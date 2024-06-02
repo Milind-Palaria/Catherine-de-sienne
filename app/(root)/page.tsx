01c65a009d4fb66ccd72325286d4973d2ee1a866
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-02 09:21:11 +0530

    Home Header Added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,25 @@
 import MainHero from '@/components/MainHero'
-import Parallax from '@/components/Parallax'
-// import Heyo from '@/components/heyo'
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Milind', lastName: 'Palaria', email: 'palaria23@gmail.com' };
   return (
       <div>
             <MainHero/>
-     
+            <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      </section>
     </div>
   )
 }
