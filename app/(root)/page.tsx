569b38d2485c693c5df3193f6f5032c8a5cade44
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-21 23:36:18 +0530

    Dynamic Login Credentials

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,11 @@ import MainHero from '@/components/MainHero'
 import HeaderBox from '@/components/HeaderBox';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import RightSidebar from '@/components/RightSidebar';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = () => {
-  const loggedIn = { firstName: 'Milind', lastName: 'Palaria', email: 'palaria23@gmail.com' };
+const Home = async () => {
+  // const loggedIn = { firstName: 'Milind', lastName: 'Palaria', email: 'palaria23@gmail.com' };
+  const loggedIn= await getLoggedInUser();
   return (
       <div>
             {/* <MainHero/> */}
@@ -14,7 +16,7 @@ const Home = () => {
           <HeaderBox 
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.name || 'Guest'}
             subtext="Access and manage your account and transactions efficiently."
           />
             <TotalBalanceBox 
