b7e248d4800809b1d2aab3abcdccf9df6ccab290
milind-palaria <lit2021047@iiitl.ac.in>
2024-07-04 15:04:10 +0530

    Transaction History

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import Footer from './Footer'
+import PlaidLink from './PlaidLink'
 
 const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname();
@@ -14,7 +15,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
         <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
-          <Image 
+          <Image
             src="/icons/logo.svg"
             width={34}
             height={34}
@@ -32,7 +33,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
               className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
             >
               <div className="relative size-6">
-                <Image 
+                <Image
                   src={item.imgURL}
                   alt={item.label}
                   fill
@@ -48,10 +49,10 @@ const Sidebar = ({ user }: SiderbarProps) => {
           )
         })}
 
-        USER
+        <PlaidLink user={user} />
       </nav>
 
-      <Footer user={user}/>
+      <Footer user={user} />
     </section>
   )
 }
