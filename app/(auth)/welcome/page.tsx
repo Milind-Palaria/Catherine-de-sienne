478ddea3837857f708b831ba5a57bff6583b6435
milind-palaria <lit2021047@iiitl.ac.in>
2024-07-06 18:07:07 +0530

    Aligning & Responsiveness

--- app/(auth)/welcome/page.tsx
+++ app/(auth)/welcome/page.tsx
@@ -23,7 +23,7 @@ import { GlobeDemo } from '@/components/ui/GlobeUse'
 //   },
 // ];
 
-const words = ["Modern.", "Smart.", "Easy.", "Luxirious."];
+const words = ["Modern.", "Smart.", "Easy.", "Luxurious."];
 const Welcome = () => {
   return (
     <section className="relative bg-[#0D090A] h-screen w-full">
@@ -32,22 +32,25 @@ const Welcome = () => {
           
           {/* <br /> */}
           {/* <br /> */}
-          <span>
-            <span className='text-[10vw]  text-[#F8F4E1] leading-[4rem]'>Catherine <span> De Sienne</span></span>
+          <div className='flex max-md:flex-col justify-center items-center lg:gap-10'>
+            <span className='max-md:text-[20vw] text-[10vw] text-[#F8F4E1] leading-[4rem]'>Catherine </span>
+            <span className='max-md:text-[15vw] text-[10vw] text-[#F8F4E1] leading-[4rem] max-md:pt-3'> De </span>
+            <span className='max-md:text-[20vw] text-[10vw] text-[#F8F4E1] leading-[4rem]'> Sienne</span>
+            
 
-          </span>
-          <div className=' tracking-wide opacity-90'>
+          </div>
+          <div className='pt-10 max-md:pt-4 tracking-wide opacity-90'>
 
             banking made
             <FlipWords words={words} />
           </div>
         </div>
         {/* <TypewriterEffectSmooth words={words} /> */}
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-5 font-semibold tracking-wider">
-          <button className="w-40 h-10 rounded-xl bg-black border border-white  text-white text-sm">
+        <div className="flex flex-row space-y-0 space-x-4 mt-5 font-semibold tracking-wider justify-center items-center">
+          <button className="max-md:w-24 w-40 text-black max-md:text-[0.75rem] text-sm max-md:h-[1.75rem] h-10 max-md:rounded-md rounded-xl  bg-black border border-white  text-white">
             Join now
           </button>
-          <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+          <button className="max-md:w-24 w-40 bg-white text-black border border-black max-md:text-[0.75rem]  text-sm max-md:h-[1.75rem] h-10 max-md:rounded-md rounded-xl ">
             Login
           </button>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,9 @@ const config = {
           1: "rgba(255, 255, 255, 0.10)",
         },
         bankGradient: "#0179FE",
+        backgroundImage: {
+          'custom-gradient': 'linear-gradient(180.2deg, #785589 -6.9%, #23091F 76.7%)',
+        },
         indigo: {
           500: "#6172F3",
           700: "#3538CD",
