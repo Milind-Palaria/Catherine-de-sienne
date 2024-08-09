23ed149304a04606789451b947eafccceae2c796
milind-palaria <lit2021047@iiitl.ac.in>
2024-08-10 04:24:28 +0530

    Optimizing Client Wrapper

--- components/ClientWrapper.tsx
+++ components/ClientWrapper.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import ClientComponent from './ClientComponent';
 
 export default function ClientWrapper({
   children,
@@ -14,11 +13,11 @@ export default function ClientWrapper({
     const stopLoader = () => setLoading(false);
 
     const maxLoadTime = setTimeout(stopLoader, 10000);
-
     const handleLoad = () => {
       clearTimeout(maxLoadTime);
       setTimeout(stopLoader, 5000);
     };
+
     if (document.readyState === 'complete') {
       handleLoad();
     } else {
@@ -31,36 +30,46 @@ export default function ClientWrapper({
     };
   }, []);
 
-  if (loading) {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100vh',
-        }}
-      >
+  return (
+    <div>
+      {loading && (
         <div
           style={{
-            border: '4px solid rgba(0, 0, 0, 0.1)',
-            borderLeftColor: '#000',
-            borderRadius: '50%',
-            width: '40px',
-            height: '40px',
-            animation: 'spin 1s linear infinite',
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#fff',
+            zIndex: 9999, // Ensure loader is on top
           }}
-        />
-        <style jsx>{`
-          @keyframes spin {
-            to {
-              transform: rotate(360deg);
+        >
+          <div
+            style={{
+              border: '4px solid rgba(0, 0, 0, 0.1)',
+              borderLeftColor: '#000',
+              borderRadius: '50%',
+              width: '40px',
+              height: '40px',
+              animation: 'spin 1s linear infinite',
+            }}
+          />
+          <style jsx>{`
+            @keyframes spin {
+              to {
+                transform: rotate(360deg);
+              }
             }
-          }
-        `}</style>
-      </div>
-    );
-  }
+          `}</style>
+        </div>
+      )}
 
-  return <ClientComponent>{children}</ClientComponent>;
+      <div style={{ visibility: loading ? 'hidden' : 'visible' }}>
+        {children}
+      </div>
+    </div>
+  );
 }
