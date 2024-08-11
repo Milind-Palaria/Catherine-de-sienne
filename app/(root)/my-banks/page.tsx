be8864ec52e86900ef93be3d79dc82973ff9aae1
milind-palaria <lit2021047@iiitl.ac.in>
2024-08-11 23:35:18 +0530

    Bank Card ui finalization #2

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -39,7 +39,7 @@ const MyBanks = async () => {
     //     </div>
     //   </div>
     // </section>
-    <section className='home min-h-[100vh] pb-10 relative bg-[#0D090A] max-md:px-4 !overflow-x-hidden'>
+    <section className='home min-h-[100vh] relative bg-[#0D090A] max-md:px-4 !overflow-x-hidden'>
       <DotPattern
         className={cn(
           "[mask-image:radial-gradient(70vh_circle_at_center,white,transparent)] !z-0",
@@ -52,10 +52,10 @@ const MyBanks = async () => {
           <MyBanksLamp user={loggedIn?.firstName || 'Guest'} />
 
           {/* <div className="mt-[25vh] md:mt-[30vh]"> */}
-          <div className="relative z-[2] mt-[25vh] md:mt-[35vh] mx-[20vw] pb-7 ">
+          <div className="relative z-[2] mt-[25vh] md:mt-[32vh] mx-[20vw] pb-7 ">
 
-            <h2 className="text-xl md:text-5xl text-[rgba(0,255,255,0.45)] font-medium font-secondary text-center mb-3">
-              Your cards
+            <h2 className="bg-[#000]/30 backdrop-blur-md border border-white/30 rounded-md max-w-[35vw] md:max-w-[20vw] xl:max-w-[15vw] p-[2vw] md:p-[1vw] text-xl md:text-3xl text-[rgba(0,255,255,0.45)] font-medium font-secondary text-center m-auto mb-[2vh] md:mb-[4vh]">
+              Accounts
             </h2>
             <div className="flex items-center justify-center flex-wrap gap-6 md:gap-10">
               {accounts && accounts.data.map((a: Account) => (
